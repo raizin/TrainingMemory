e1b2b1158024057ce92f9fa48b3227ec36b96973
Amasaki Raijin <raizin64@fmail.com>
2013-10-04 21:11:36 +0900

    自作の関数でカード画像を２枚表示させるところまで および画像の追加

--- TrainingMemory/ViewController.m
+++ TrainingMemory/ViewController.m
@@ -14,20 +14,59 @@
 
 @implementation ViewController
 
+
+// カードレイヤー(※自作関数)
+- (CALayer *)makeCardLayerAtPos: (CGPoint)pos name:(NSString *)name
+{
+  CATransform3D perspective = CATransform3DIdentity;
+  perspective.m34 = -1 / 150.0;
+  
+  CALayer *cardLayer = [CALayer layer];
+  cardLayer.bounds = CGRectMake(0, 0, 100, 100);
+  cardLayer.position = pos;
+  cardLayer.sublayerTransform = perspective;
+  cardLayer.name = name;
+  
+  CALayer *topLayer = [CALayer layer];
+  topLayer.bounds = CGRectMake(0, 0, 100, 100);
+  topLayer.position = CGPointMake(50, 50);
+  topLayer.contents = (id)[UIImage imageNamed:@"card_top.png"].CGImage;
+  topLayer.zPosition = 1;
+  
+  CALayer *backLayer = [CALayer layer];
+  backLayer.bounds = CGRectMake(0, 0, 100, 100);
+  backLayer.position = CGPointMake(50, 50);
+  backLayer.contents = (id)[UIImage imageNamed:[name stringByAppendingPathExtension:@"png"]].CGImage;
+  backLayer.zPosition = 0;
+  backLayer.transform = CATransform3DMakeRotation(M_PI, 0.0, 1.0, 0.0);
+  
+  [cardLayer addSublayer:topLayer];
+  [cardLayer addSublayer:backLayer];
+ 
+  return cardLayer;
+}
+
+
+// 画面が表示される直前に呼び出されるメソッド
+- (void)viewWillAppear:(BOOL)animated
+{
+  self.view.layer.contents = (id)[UIImage imageNamed:@"bg01.png"].CGImage; // 背景
+  
+  CALayer *layer1 = [self makeCardLayerAtPos:CGPointMake(100, 100) name:@"card1"];
+  CALayer *layer2 = [self makeCardLayerAtPos:CGPointMake(200, 200) name:@"card1"];
+  
+  [self.view.layer addSublayer:layer1];
+  [self.view.layer addSublayer:layer2];
+  
+}
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
-
-
-// 画面が表示される直前に呼び出されるメソッド　
-- (void)viewWillAppear:(BOOL)animated
-{
-  self.view.layer.contents = (id)[UIImage imageNamed:@"bg01.png"].CGImage;
-
-}
-
 
 - (void)didReceiveMemoryWarning
 {
