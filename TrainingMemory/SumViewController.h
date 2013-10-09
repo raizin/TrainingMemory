86aa5e7142ef727f25531704bbf813b2a35c37e0
Amasaki Raijin <raizin64@fmail.com>
2013-10-10 00:34:58 +0900

    集計用Viewファイルの追加

---
+++ TrainingMemory/SumViewController.h
@@ -0,0 +1,13 @@
+//
+//  SumViewController.h
+//  TrainingMemory
+//
+//  Created by z on 2013/10/10.
+//  Copyright (c) 2013 FoceSystemSolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SumViewController : UIViewController
+
+@end
---
+++ TrainingMemory/SumViewController.m
@@ -0,0 +1,38 @@
+//
+//  SumViewController.m
+//  TrainingMemory
+//
+//  Created by z on 2013/10/10.
+//  Copyright (c) 2013 FoceSystemSolution. All rights reserved.
+//
+
+#import "SumViewController.h"
+
+@interface SumViewController ()
+
+@end
+
+@implementation SumViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
